8a96ed56f56f1ce80196f6a6dde1f7c08087ab27
Kaaydenn <105562112+Kaaydenn@users.noreply.github.com>
2023-10-18 21:50:16 +0000

    models

--- PandasAI/home/models.py
+++ PandasAI/home/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class Publicacion(models.Model):
+    titulo = models.CharField(max_length=200)
+    contenido = models.TextField()
+    imagen = models.ImageField(upload_to='imagenes/', blank=True, null=True)
+
+    def __str__(self):
+        return self.titulo
