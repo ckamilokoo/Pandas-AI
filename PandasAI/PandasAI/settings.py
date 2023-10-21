7c343d4a1985f89b29516a454b27775099d3cfac
Kaaydenn <nanoshiro0@gmail.com>
2023-10-21 13:57:25 -0300

    aplicacion login

--- PandasAI/PandasAI/settings.py
+++ PandasAI/PandasAI/settings.py
@@ -39,8 +39,13 @@ INSTALLED_APPS = [
     "django.contrib.messages",
     "django.contrib.staticfiles",
     "home",
+    "login",
+    "crispy_forms",
+
     
 ]
+
+CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
 MIDDLEWARE = [
     "django.middleware.security.SecurityMiddleware",
--- PandasAI/PandasAI/urls.py
+++ PandasAI/PandasAI/urls.py
@@ -22,9 +22,5 @@ from django.contrib.auth import views as auth_views
 urlpatterns = [
     path("admin/", admin.site.urls),
     path('', include('home.urls')),
-    path('login/', auth_views.LoginView.as_view(template_name='registration/login.html'), name='login'),
-    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
-    path('registro/', auth_views.RegistrationView.as_view(template_name='registration/registration_form.html'), name='registro'),
-    path('accounts/', include('registration.backends.default.urls')),
-
+    path('login/',include('login.urls')),
 ]
