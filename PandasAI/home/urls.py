6af7e2d6595b09e3ad199d903d3e2d0a881cc526
Kaaydenn <nanoshiro0@gmail.com>
2023-10-20 22:38:01 -0300

    crud, login en proceso

--- PandasAI/home/urls.py
+++ PandasAI/home/urls.py
@@ -3,4 +3,9 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name='home'),  # Asocia la vista 'home' a la URL raíz
+    path('crear_publicacion', views.crear_publicacion, name='crear_publicacion'),
+    path('listar_publicacion/', views.listar_publicacion, name='listar_publicacion'),
+    path('eliminar_publicacion/<int:publicacion_id>/', views.eliminar_publicacion, name='eliminar_publicacion'),
+    path('actualizar_publicacion/<int:publicacion_id>/', views.actualizar_publicacion, name='actualizar_publicacion'),
+    
 ]
--- PandasAI/home/views.py
+++ PandasAI/home/views.py
@@ -1,9 +1,52 @@
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404, redirect, render
+
+from .models import Publicacion
+from .forms import PublicacionForm  # Asegúrate de tener el formulario definido en forms.py
 
 # Create your views here.
 
 #función para ver la página home 
+#publicaciones crud
 
 def home(request):
-    return render(request, 'home/home.html')
+    publicaciones = Publicacion.objects.all()
+    return render(request, 'home/home.html', {'publicaciones': publicaciones})
 
+def crear_publicacion(request):
+    if request.method == 'POST':
+        form = PublicacionForm(request.POST)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_publicacion')  # Redirige a la vista de listar publicaciones
+    else:
+        form = PublicacionForm()
+
+    return render(request, 'home/publicacion/crear_publicacion.html', {'form': form})
+
+def actualizar_publicacion(request, publicacion_id):
+    publicacion = get_object_or_404(Publicacion, pk=publicacion_id)
+    
+    if request.method == 'POST':
+        form = PublicacionForm(request.POST, instance=publicacion)
+        if form.is_valid():
+            form.save()
+            return redirect('listar_publicacion')  # Redirige a la vista de listar publicaciones
+    else:
+        form = PublicacionForm(instance=publicacion)
+
+    return render(request, 'home/publicacion/actualizar_publicacion.html', {'form': form, 'publicacion': publicacion})
+
+def listar_publicacion(request):
+    publicaciones = Publicacion.objects.all()
+    return render(request, 'home/publicacion/listar_publicacion.html', {'publicaciones': publicaciones})
+
+def eliminar_publicacion(request, publicacion_id):
+    if request.method == 'POST':
+        publicacion = get_object_or_404(Publicacion, pk=publicacion_id)
+        publicacion.delete()
+        return redirect('listar_publicacion')  # Redirige a la vista de listado de publicaciones
+    else:
+        # Maneja posibles solicitudes GET de forma adecuada (puedes mostrar un mensaje o redirigir a otra página).
+        pass
+    
+#
