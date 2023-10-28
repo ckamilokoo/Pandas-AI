b00a93316774372fdd0470232d7e3ec4d8f9b224
Alex Pérez Argomedo <a.perez.argomedo@gmail.com>
2023-10-28 19:07:17 -0300

    Urls

--- PandasAI/PandasAI/urls.py
+++ PandasAI/PandasAI/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include 
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 
 urlpatterns = [
@@ -28,3 +30,6 @@ urlpatterns = [
     path('instructivo/', include('instructivo.urls')),
     path('subscripcion/', include('subscripcion.urls')),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
