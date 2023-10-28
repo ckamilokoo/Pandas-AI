c9bfd87d89e7f264c2569ced88d51600fc9a3703
camilo campos <128047188+ckamilokoo2@users.noreply.github.com>
2023-10-28 17:21:11 -0300

    toques finales a subscripcion

--- PandasAI/subscripcion/views.py
+++ PandasAI/subscripcion/views.py
@@ -12,15 +12,16 @@ def actualizar_nivel(request, nuevo_nivel):
     # Obtén el usuario actual
     user = request.user
 
-    # Verifica si el usuario es un usuario normal y el nuevo nivel es válido
-    if user.nivel == 1 and nuevo_nivel in [2, 3]:
+    # Verifica si el usuario es un usuario normal y el nuevo nivel es válido para subir a nivel 2
+    if user.nivel == 1 and nuevo_nivel == 2:
         user.nivel = nuevo_nivel
         user.save()
-        messages.success(request, f"Tu nivel se ha actualizado a Nivel {nuevo_nivel}.")
-    elif user.nivel in [2, 3] and nuevo_nivel == 1:
+        messages.success(request, "Tu nivel se ha actualizado a Usuario Nivel 2.")
+    # Verifica si el usuario está en nivel 1 o 2 y el nuevo nivel es 3
+    elif user.nivel in [1, 2] and nuevo_nivel == 3:
         user.nivel = nuevo_nivel
         user.save()
-        messages.success(request, "Tu nivel se ha actualizado a Usuario Normal.")
+        messages.success(request, "¡Felicidades! Has avanzado a Usuario Nivel 3.")
     else:
         messages.error(request, "No puedes actualizar tu nivel en este momento.")
 
@@ -43,12 +44,8 @@ def cancelar_nivel(request):
     return redirect('home')
 
 
-
-
-
-
-
 # Create your views here.
+@login_required
 def subscripcion(request):
     return render(request, 'subscripcion/subscripcion.html')
 
