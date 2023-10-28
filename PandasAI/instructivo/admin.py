f2e0b28a1131a066a42c47e5317b3e69cab6d2c1
Alex Pérez Argomedo <a.perez.argomedo@gmail.com>
2023-10-28 18:40:09 -0300

    Django admin

--- PandasAI/instructivo/admin.py
+++ PandasAI/instructivo/admin.py
@@ -1,8 +1,11 @@
 from django.contrib import admin
 from .models import Instruccion
+from django.contrib.auth.models import User
 
 
 # Register your models here.
 @admin.register(Instruccion)
 class InstruccionAdmin(admin.ModelAdmin):
     list_display = ('titulo', )  # Campos que se mostrarán en la lista de instrucciones
+
+admin.site.register(User)
--- PandasAI/login/admin.py
+++ PandasAI/login/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from django.contrib.auth.models import User  # Importa el modelo de usuario aquí
+from .models import CustomUser  # Asegúrate de importar tu modelo de usuario personalizado
 
-# Register your models here.
+
+
+admin.site.register(User)  # Registra el modelo de usuario
+admin.site.register(CustomUser)  # Registra tu modelo de usuario personalizado
